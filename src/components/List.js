4255bf930f3fd0c4f90cc9f4af541042432c2cec
Kathrin Noack <KN82@gmx.de>
2019-07-26 15:01:08 +0200

    add styling of list

--- src/components/List.js
+++ src/components/List.js
@@ -1,26 +1,53 @@
 import React from "react";
 import styled from "styled-components";
-import GlobalStyles from "../app/GlobalStyles";
 
 const StyledList = styled.div`
+  display: flex;
+  flex-direction: column;
   background-color: #fffcf2;
   color: #4b5454;
   border: #4b5454 solid 2px;
+  font-size: 15px;
   border-radius: 25px;
-  padding: 5px 15px 5px 15px;
+  margin: 10px;
+  padding: 10px 15px 10px 15px;
 `;
 
-const StyledDate = styled.p``;
+const StyledListHead = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
-function List() {
+const StyledDate = styled.div``;
+
+const StyledAmount = styled.div``;
+
+const StyledDescription = styled.p``;
+
+const StyledTag = styled.button`
+  background-color: #4b5454;
+  color: #fffcf2;
+  border-radius: 25px;
+  margin-left: 5px;
+`;
+
+function List({ date, amount, description, tag }) {
   return (
-    <StyledList>
-      <>
-        <p>10.07.2019 </p>
-        <p> - 12,50 </p>
-        <p>Edeka, Lebensmittel</p>
-      </>
-    </StyledList>
+    <>
+      <StyledList>
+        <>
+          <StyledListHead>
+            <StyledDate>{date}</StyledDate>
+            <StyledAmount>{amount}</StyledAmount>
+          </StyledListHead>
+          <StyledDescription>
+            {description}
+            <StyledTag>{tag}</StyledTag>
+          </StyledDescription>
+        </>
+      </StyledList>
+    </>
   );
 }
 
