b8c5f4f05073617ae8c47bb674ed99656d50253e
Kathrin Noack <KN82@gmx.de>
2019-07-29 16:49:35 +0200

    style Header and add icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,33 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   background-color: #4b5454;
+  padding: 5px;
+`;
+
+const StyledHeadTitle = styled.h1`
   color: #fffcf2;
-  font-size: 20px;
+  font-size: 30px;
+  margin: 0px;
+`;
+
+const StyledIcon = styled.img`
+  height: 65px;
+  width: 60px;
+  margin-left: 25px;
 `;
 
 function Header() {
-  return <StyledHeader>My Money</StyledHeader>;
+  return (
+    <StyledHeader>
+      <StyledHeadTitle>My Money</StyledHeadTitle>
+      <StyledIcon src="Icon.png" alt="Geldsack" />
+    </StyledHeader>
+  );
 }
 
 export default Header;
