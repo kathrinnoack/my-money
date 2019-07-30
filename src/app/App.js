438d7fba273a8e61f7bb1851dac34c0ec3fd2467
Kathrin Noack <KN82@gmx.de>
2019-07-30 16:21:36 +0200

    add libary dinero

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,10 @@ import List from "../pages/List";
 import Header from "../components/Header";
 import Saldo from "../components/Saldo";
 import CalculateSaldo from "../components/CalculateSaldo";
+import Dinero from "dinero.js";
+
+const price = Dinero({ amount: 5000, currency: "EUR" });
+console.log(price.toFormat("$0,0.00"));
 
 function App({ title }) {
   return (
--- src/components/CalculateSaldo.js
+++ src/components/CalculateSaldo.js
@@ -1,10 +1,15 @@
 import React from "react";
 import listData from "../pages/__mock__/list.json";
+import Dinero from "dinero.js";
 
 function CalculateSaldo() {
   const [list, setList] = React.useState(listData);
-
-  console.log(list.map(element => element.amount));
+  const total = list
+    .map(element => parseInt(element.amount * 10))
+    .reduce((acc, current) => {
+      return acc.add(Dinero({ amount: current }));
+    }, Dinero({ amount: 0 }));
+  console.log(total.toFormat("$0,0.00"));
   return <div>{list.map(element => element.amount)}</div>;
 }
 
