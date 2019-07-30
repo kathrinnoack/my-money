20ed5344c66faebdd616e16c16af9a4304224136
Kathrin Noack <KN82@gmx.de>
2019-07-30 11:27:43 +0200

    add styling fo saldo

--- src/components/Saldo.js
+++ src/components/Saldo.js
@@ -4,10 +4,11 @@ import styled from "styled-components";
 const StyledSaldo = styled.div`
   display: flex;
   justify-content: space-between;
-  font-size: 20px;
+  border-top: #4b5454 solid 4px;
+  font-size: 22px;
   font-weight: bold;
-  margin: 10px;
-  padding: 10px 15px 10px 15px;
+  margin: 15px 10px 15px 10px;
+  padding: 0 15px 0 15px;
 `;
 
 const StyledSaldoTitle = styled.p`
