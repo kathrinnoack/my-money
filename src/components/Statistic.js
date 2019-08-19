d4cd8c56402823b9afbb38563d969d14951c792e
Kathrin Noack <KN82@gmx.de>
2019-08-19 17:57:12 +0200

    clean up

--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -7,24 +7,18 @@ const StatisticHeadline = styled.h3`
   margin: 10px;
 `;
 
-export const WhiteSpace = styled.div`
-  width: 100%;
-  height: 10px;
-`;
-
 function Statistic({ transactions, handleMonth, handleCategory }) {
   console.log(transactions);
 
   return (
     <>
       <StatisticHeadline>Auswahl:</StatisticHeadline>
-      <WhiteSpace />
       <SelectMonth
         handleMonth={handleMonth}
         transactions={transactions}
         handleCategory={handleCategory}
       />
-      <WhiteSpace />
+
       <CheckCategory
         handleCategory={handleCategory}
         transactions={transactions}
